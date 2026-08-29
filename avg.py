4f1d137e07492ba0ed7ab958c3c2ad41d2b03353
Vinit kumar <vinitkvd09@gmail.com>
2026-08-28 20:35:16 -0700

    Adde new file in avg

---
+++ avg.py
@@ -0,0 +1,13 @@
+# 5 students data % nikalna 
+
+a = int(input("enter the first number:"))
+b = int(input("enter the second number:"))
+c = int(input("enter the third number:"))
+d = int(input("enter the four numbeeer:"))
+e = int(input("enter the five number:"))
+
+avg = (a+b+c+d+e)/5
+
+print("Find The Student parsantage",avg)
+
+# shan bhai es file ma kuch change karo hona avg hi chiya
