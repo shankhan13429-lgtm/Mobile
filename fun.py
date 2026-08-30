29a2ff48eee2b443fe3eb5afd72c91a7ed2f0998
Vinit kumar <vinitkvd09@gmail.com>
2026-08-30 08:27:01 -0700

    aded new file in avg

---
+++ fun.py
@@ -0,0 +1,41 @@
+# hyyy 5 students marks avg in  python ......
+
+a = 70 
+b = 80
+c = 77
+d = 90
+e = 89
+avg = (a+b+c+d+e)/5
+print(avg)
+
+a = 90
+b = 88
+c = 70
+d = 78
+e = 90
+avg = (a+b+c+d+e)/5
+print(avg)
+
+a = 56 
+b = 90
+c = 95
+d = 80
+e = 76
+avg = (a+b+c+d+e)/5
+print(avg)
+
+a = 75 
+b = 85
+c = 90
+d = 67
+e = 88
+avg = (a+b+c+d+e)/5
+print(avg)
+
+a = 89 
+b = 86
+c = 77
+d = 90
+e = 89
+avg = (a+b+c+d+e)/5
+print(avg)
